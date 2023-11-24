e18306a8f07a5e2a5eafa1f4e35f1813d58ba16d
= <=>
2023-11-24 13:08:18 +0100

    initial commit

---
+++ src/main/java/com/techu/apitechu/ApitechuApplication.java
@@ -0,0 +1,13 @@
+package com.techu.apitechu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApitechuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApitechuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/techu/apitechu/ApitechuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techu.apitechu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApitechuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
